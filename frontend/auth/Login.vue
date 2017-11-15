1aa333d00fcafbe120641b191166bb8000f3ebab
Prodev2017 <chris.bergman320@gmail.com>
2017-11-15 10:44:19 +0800

    add login form

--- frontend/auth/Login.vue
+++ frontend/auth/Login.vue
@@ -1,5 +1,17 @@
 <template>
-
+	<form class="tab">
+		<div class="inpt_w">
+			<input type="text" placeholder="Username" v-model="credentials.username" class="inpt i-user"><label></label>
+		</div>
+		<div class="inpt_w">
+			<input type="password" placeholder="Password" v-model="credentials.password" class="inpt i-pass"><label></label>
+		</div>
+		<div v-for="error in Object.keys(errors)">
+			 <i class="el-icon-warning"></i> {{error}}: {{errors[error][0]}}
+		</div>
+		<div class="submit-button" @click="login">Войти</div>
+		<a class="forgot" href="">Забыли пароль?</a>
+	</form>
 </template>
 
 <script>
