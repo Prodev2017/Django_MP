8a6267562b39919542c857fc3b8e3787f189b096
Prodev2017 <chris.bergman320@gmail.com>
2017-11-15 10:45:29 +0800

    add signup form

--- frontend/auth/Signup.vue
+++ frontend/auth/Signup.vue
@@ -1,7 +1,3 @@
-<template>
-
-</template>
-
 <script>
 import auth from '../auth'
 
@@ -19,6 +15,24 @@ export default {
   }
 }
 </script>
+
+<template>
+	<form class="tab">
+		<div class="inpt_w">
+			<input type="text" placeholder="Username" v-model="credentials.username"class="inpt i-user"><label></label>
+		</div>
+		<div class="inpt_w">
+			<input type="password" placeholder="Password" v-model="credentials.password" class="inpt i-pass"><label></label>
+		</div>
+		<div v-for="error in Object.keys(errors)">
+			 <i class="el-icon-warning"></i> {{error}}: {{errors[error][0]}}
+		</div>
+		<div class="submit-button" @click="signUp">Зарегистрироваться</div>
+		<a class="forgot" href="">Забыли пароль?</a>
+	</form>
+</template>
+
+
 
 <style lang="scss" scoped>
 </style>
